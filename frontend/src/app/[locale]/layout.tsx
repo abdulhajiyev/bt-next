502afce6d0af85ea1e9344862dcc2604b3ca6bc7
Abdul Hajiyev <abdulhajiyev@gmail.com>
2023-12-04 13:40:09 +0400

    Fix hydration error

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,42 +1,18 @@
-import { Poppins } from "next/font/google";
-import SiteHeader from "../(client-components)/(Header)/SiteHeader";
-import ClientCommons from "../ClientCommons";
-import "../globals.css";
-import "@/fonts/line-awesome-1.3.0/css/line-awesome.css";
-import "@/styles/index.scss";
-import "rc-slider/assets/index.css";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
-import Footer from "@/components/Footer";
-import FooterNav from "@/components/FooterNav";
-import { Metadata } from "next";
-import { I18nProviderClient } from "locales/client";
-import React from "react";
+'use client';
+import { I18nProviderClient } from 'locales/client';
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["300", "400", "500", "600", "700"],
-});
-
-export const metadata: Metadata = {
-  title: "B&T - Booking online",
-  description: "Booking online",
-  keywords: "Booking online",
-
-};
-
-export default function RootLayout({ params: { locale }, children }: { params: { locale: string }, children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
   return (
-    <I18nProviderClient locale="az">
-      <html className={poppins.className}>
-        <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
-          <ClientCommons />
-          <SiteHeader />
-          {children}
-          <FooterNav />
-          <Footer />
-        </body>
-      </html>
-    </I18nProviderClient>
+    <>
+      <I18nProviderClient locale={locale}>
+        <main className="flex-1">{children}</main>
+      </I18nProviderClient>
+    </>
   );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import SiteHeader from "./(client-components)/(Header)/SiteHeader";
 import ClientCommons from "./ClientCommons";
 import "./globals.css";
 import "@/fonts/line-awesome-1.3.0/css/line-awesome.css";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 import "@/styles/index.scss";
 import "rc-slider/assets/index.css";
 import Footer from "@/components/Footer";
