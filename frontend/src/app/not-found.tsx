2d5c06db383b2d8fcba2a9cfc61944b33aed8e9a
Abdul Hajiyev <abdulhajiyev@gmail.com>
2023-12-04 22:13:31 +0400

    getScopedI18n

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -2,9 +2,9 @@ import React from "react";
 // import I404Png from "@/images/404.png";
 // import Image from "next/image";
 // import ButtonPrimary from "@/shared/ButtonPrimary";
-import { getI18n } from "locales/server";
+import { getScopedI18n } from "locales/server";
 
 export default async function NotFound() {
-  const t = await getI18n();
-  return <>Not Found {t("hero")}</>
+  const t = await getScopedI18n("hello");
+  return <>Not Found {t("world")}</>
 }
