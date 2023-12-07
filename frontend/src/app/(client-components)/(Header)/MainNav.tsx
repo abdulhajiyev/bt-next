eefb165e597721d7511aeea503361d094d956539
Abdul Hajiyev <abdulhajiyev@gmail.com>
2023-12-07 22:26:53 +0400

    Revert "Fixed some sht in header"
    This reverts commit 468da0570548b4f8498ac25566bc1c2391b5593f.

--- frontend/src/app/(client-components)/(Header)/MainNav.tsx
+++ frontend/src/app/(client-components)/(Header)/MainNav.tsx
@@ -16,42 +16,40 @@ const MainNav: FC<MainNavProps> = ({ className = "" }) => {
   return (
     <div className={`nc-MainNav1 relative z-10 ${className}`}>
       <div className="px-4 lg:container py-4 relative flex justify-between items-center">
-        <div className="md:flex justify-start flex-1 space-x-4 sm:space-x-10">
-          <Logo className="md:w-24 w-16 self-center" />
+        <div className="hidden md:flex justify-start flex-1 space-x-4 sm:space-x-10">
+          <Logo className="w-20 self-center" />
           <Navigation />
         </div>
 
-        {/* <div className="flex lg:hidden flex-[3] max-w-lg !mx-auto md:px-3 ">
+        <div className="flex lg:hidden flex-[3] max-w-lg !mx-auto md:px-3 ">
           <div className="self-center flex-1">
             <HeroSearchForm2MobileFactory />
           </div>
-        </div> */}
+        </div>
 
-        <div className="flex md:flex flex-shrink-0 justify-end lg:flex-none text-neutral-700 dark:text-neutral-100 items-center">
-          <div className="xl:flex">
+        <div className="hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
+          <div className="hidden xl:flex space-x-0.5">
             <div>
-              <a
-                href="https://wa.me/994502009999"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <ButtonPrimary icon="lab la-whatsapp">+994502009999</ButtonPrimary>
-              </a>
-            </div>
-            <div className="hidden xl:flex">
-              <div className="px-1" />
-              <SwitchDarkMode />
-              <LangDropdown
-                className="flex"
-                panelClassName="z-10 w-screen max-w-[280px] px-4 mb-3 right-3 bottom-full sm:px-0"
-              />
-            </div>
+            <a
+              href="https://wa.me/994502009999"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ButtonPrimary icon="lab la-whatsapp">+994502009999</ButtonPrimary>
+            </a>
+          </div>
+            <div className="px-1" />
+            <SwitchDarkMode />
+            <LangDropdown
+          className="flex"
+          panelClassName="z-10 w-screen max-w-[280px] px-4 mb-3 right-3 bottom-full sm:px-0"
+        />
           </div>
 
           <div className="flex xl:hidden items-center">
-            {/* <SwitchDarkMode className="hidden sm:flex"/> */}
+            <SwitchDarkMode />
             <div className="px-0.5" />
-            <MenuBar className="hakuna"/>
+            <MenuBar />
           </div>
         </div>
       </div>
