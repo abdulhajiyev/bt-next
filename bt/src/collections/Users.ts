d1cef41203cb541102ab4654588c539cd8b94801
Abdul Hajiyev <abdulhajiyev@gmail.com>
2024-02-12 14:26:31 +0400

    user access

--- bt/src/collections/Users.ts
+++ bt/src/collections/Users.ts
@@ -1,15 +1,21 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from "payload/types";
 
 const Users: CollectionConfig = {
-  slug: 'users',
-  auth: true,
-  admin: {
-    useAsTitle: 'email',
-  },
-  fields: [
-    // Email added by default
-    // Add more fields as needed
-  ],
-}
+	slug: "users",
+	auth: true,
+	access: {
+		create: () => true,
+		read: () => true,
+		update: () => true,
+		delete: () => true,
+	},
+	admin: {
+		useAsTitle: "email",
+	},
+	fields: [
+		// Email added by default
+		// Add more fields as needed
+	],
+};
 
-export default Users
+export default Users;
