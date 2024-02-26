373e22d5f44eaa1db673cb6bd3899d02cc9883c9
Abdul Hajiyev <abdulhajiyev@gmail.com>
2024-02-26 10:40:09 +0400

    Included in price

--- frontend/src/app/[locale]/tours/[slug]/page.tsx
+++ frontend/src/app/[locale]/tours/[slug]/page.tsx
@@ -182,7 +182,7 @@ const Page: FC<ListingExperiencesDetailPageProps> = ({ params }) => {
 		return (
 			<div className="listingSection__wrap">
 				<div>
-					<h2 className="text-2xl font-semibold">Qiymətə daxildir</h2>
+					<h2 className="text-2xl font-semibold">{td("includedInPrice")}</h2>
 				</div>
 				<div className="w-14 border-b border-neutral-200 dark:border-neutral-700" />
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-6 text-sm text-neutral-700 dark:text-neutral-300 ">
